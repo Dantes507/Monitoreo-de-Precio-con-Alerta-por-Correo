a01166d807d8f9331b4f3cef49b272e14c728fa2
David Ortega <david2312507@gmail.com>
2025-08-12 00:35:10 -0500

    Varios productos y Mejora en la estructura del mensaje

--- Monitoreo_Precios_Correo.py
+++ Monitoreo_Precios_Correo.py
@@ -1,63 +1,111 @@
 
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
-import smtplib,os
+import smtplib, os, csv, time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from email.mime.text import MIMEText
 
 
-def mandarEmail(precio_umbral,precio_pagina,emisor,password,receptor,producto):
-    if precio_pagina < precio_umbral:
-       porcentaje = 100 - ((precio_pagina*100)/precio_Minimo)
-       #print(porcentaje)
-       try:
-            #Crear el objeto de conexion y especifica el puerto
-            conexionEmail = smtplib.SMTP("smtp.gmail.com", 587)
-            conexionEmail.ehlo()
-            conexionEmail.starttls()
-            #logearse
-            conexionEmail.login(emisor,password)
-            #Enviar email
-            conexionEmail.sendmail(emisor,receptor, "Subject: Precio bajo del Umbral" f"\n\n +El producto : {producto} que te interesa ahora esta a {porcentaje} % menos de tu precio deseado de {precio_Minimo}")
-            #Cerrar la conexion
-            conexionEmail.quit()
-            print("\nCorreo Enviado...")
-       except:
-           print ("\nError al enviar el email")
-           
+def mandarEmail(precios_umbral,precios_pagina,productos):
+    i = 0
+    try:
+        mensaje = """"""
+        for precio_pagina in precios_pagina:     
+            #Comprobacion del umbral del precio
+            if precio_pagina < precios_umbral[i]:
+                porcentaje = 100 - ((precio_pagina*100)/precios_umbral[i])
+                mensaje += f"\n\n +El producto : {productos[i]} que te interesa ahora esta a {porcentaje} % menos de tu precio deseado de {precios_umbral[i]} con un valor de : {precios_pagina[i]}"     
+            i+=1
+        i=0
+         
+        #Cargar variables del archivo .env
+        load_dotenv()
+        #Email que envia el mensaje
+        emisor = os.getenv("EMAIL_EMISOR")
+        #Contraseña de ese email
+        password = os.getenv("PASSWORD")
+        #Email que recibe el mensaje
+        receptor = os.getenv("EMAIL_RECEPTOR")
+        #Crear el objeto de conexion y especifica el puerto
+        conexionEmail = smtplib.SMTP("smtp.gmail.com", 587)
+        conexionEmail.ehlo()
+        conexionEmail.starttls()
+        #Creacion de la estructura del mensaje
+        msg = MIMEText(mensaje,"plain","utf-8")
+        msg["Subject"] = "Precio bajo del Umbral"
+        msg["From"] = emisor
+        msg["To"] = receptor
+        #logearse
+        conexionEmail.login(emisor,password)
+        #Enviar email
+        #Mandar el mail
+        conexionEmail.send_message(msg)
+        #Cerrar la conexion
+        conexionEmail.quit()
+        print("\nCorreo Enviado...")
+    except:
+        print ("\nError al enviar el email")           
 
-#Mercado libre usa JS, asi que tuve que usar selenium y el uso de proyecto es solamente de APRENDIZAJE
-#Cargar variables del archivo .env
-load_dotenv()
+def lectorCsv()  :
+   try:
+        #Declaracion del diccionario para trabajar
+        productos = {}
+        #Abrir el csv con la info de  las urls y precios
+        with open('productos.csv', newline='',encoding='utf-8') as csvfile:
+            #Lectura de la info en memoria
+            Lector = csv.DictReader(csvfile)
+            for fila in Lector:
+                #Introducir los valores en el diccionario
+                productos[fila["url"]] = float(fila["precio_umbral"])
+        #Retornar el diccionario
+        return productos
+   except:
+       print("\n Ocurrio un error en la lectura del archivo .csv")     
 
-#Url del producto
-url_Producto = os.getenv("URL_PRODUCTO")
-#Precio a comparar
-precio_Minimo = float(os.getenv("PRECIO_MINIMO"))
-#Email que envia el mensaje
-email_Emisor = os.getenv("EMAIL_EMISOR")
-#Contraseña de ese email
-password = os.getenv("PASSWORD")
-#Email que recibe el mensaje
-email_Receptor = os.getenv("EMAIL_RECEPTOR")
+def busquedaPrecios(productos_dict,driver):
+    try:
+        precios = []
+        productos = []
+        precios_umbral = []
+        
+        for url in productos_dict.keys():
+            #Busca la url que se le pasa
+            driver.get(url) 
+            #Obtener la informacion de pagina, pero con selenium - HTML
+            pagina = driver.page_source
+            #Parsear el HTML
+            soup = BeautifulSoup(pagina, "html.parser")
+            #Encontrar el nombre del producto
+            elemento = soup.find('h1', class_ = "ui-pdp-title")
+            producto = elemento.get_text(strip=True)
+            productos.append(producto)
+            #Encontramos el precio del producto, con la construccion de la pagina
+            elemento = soup.find('span', class_ = "andes-money-amount__fraction")
+            #Convertir el dato para la manipulacion
+            precio = float(elemento.get_text(strip=True))
+            precios.append(precio)
+            #Dejar el programa en pause ese tiempo para evitar, bloqueos de la pagina
+            time.sleep(5)    
+      
+        for precio in productos_dict.values():
+           precios_umbral.append(precio)
+
+        mandarEmail(precios_umbral,precios,productos)
+    except:
+        print("Ocurrio un problema con la busqueda de precios")
+
+
+#Mercado libre usa JS, asi que tuve que usar selenium y el uso de proyecto es solamente de APRENDIZAJE y estoy usando Mercado libre para practicar
+
+#Leer el csv con las Urls y los precios
+productos = {}
+#Invocar la funccion de lectura del csv y obtener esa informacion
+productos = lectorCsv()
+
 #Seleccionando navegador
 driver =  webdriver.Chrome()
-driver.get(url_Producto)   #Busca la url que se le pasa
-
-#Obtener la informacion de pagina, pero con selenium - HTML
-pagina = driver.page_source
-#Parsear el HTML
-soup = BeautifulSoup(pagina, "html.parser")
-#Encontrar el nombre del producto
-elemento = soup.find('h1', class_ = "ui-pdp-title")
-producto = elemento.get_text(strip=True)
-#Encontramos el precio del producto, con la construccion de la pagina
-elemento = soup.find('span', class_ = "andes-money-amount__fraction")
-#Convertir el dato para la manipulacion
-precio = float(elemento.get_text(strip=True))
-#Cerrar navegador
+busquedaPrecios(productos,driver)
+#Cerrar navegadorS
 driver.quit()
-
-mandarEmail(precio_Minimo,precio,email_Emisor,password,email_Receptor,producto)
 print("\nPrograma Finalizado...\n")
-
--- README.txt
+++ README.txt
@@ -1,23 +1,23 @@
 #Monitoreo de Precio y Envío de Alerta por Correo
 
 
-#Este script de Python verifica el precio de un producto especifico y compara si es menor al precio que se desea y de ser así, envía una alerta al email
-que se introduzca como receptor del mensaje.
+#Este script de Python verifica el precio de uno o varios productos y compara si es menor al precio que usa de referencia y de ser así, envía una alerta al email
+que se introduzca como receptor de ese mensaje.
 
 
 #Funcionamiento
 
 1. Abre un ventana de navegador. 
-2. Busca la pagina del producto.
-3. Obtiene el Precio y el Nombre del Producto.
-4. Verifica si se cumple la sentencia (que el precio de la pagina sea menor al mínimo aceptable)
+2. Busca las pagina de los productos.
+3. Obtiene el Precio y el Nombre de los Productos.
+4. Verifica si se cumple la sentencia (que el precio de la pagina sea menor al precio de refencia)
 5. Envía una alerta al email designado para avisar de esa baja de precio).
 
 
 #Requisitos
 
 - Python 3
-- Librerías : os, dotenv, smtplib, selenium, BeautifulSoup  (vendrían incluidas con Python)
+- Librerías : os, dotenv, smtplib, selenium, BeautifulSoup, csv, time, email  (vendrían incluidas con Python)
 - Si las librerías no vienen instaladas seguir los siguientes pasos:
 
 1. Abrir el CMD.
@@ -28,20 +28,23 @@ que se introduzca como receptor del mensaje.
 #Guia de Uso
 
 1. Crear un archivo .env con los datos requeridos y la siguiente estructura:
-URL_PRODUCTO=	#Pagina del producto.
-PRECIO_MINIMO=	#Precio mínimo que desea usar para activar la alerta.
+
 EMAIL_EMISOR=	#Email que usara para enviar el mensaje.
 PASSWORD= 	#Contraseña para el uso de aplicaciones del email que usara para enviar el mensaje.
 EMAIL_RECEPTOR=	#Email al que desea que le llegue el mensaje.
 
-2. Ejecutar el .exe y listo.
-3. Proceso Terminado
+2. Crea un archivo .csv con una columna donde tengas las Urls de los productos y en otra los precios a para evaluar.
+3. Ejecutar el .exe y listo.
+4. Proceso Terminado.
+
+
+#Extra:
+- El proyecto tiene un archivo .csv para ser probado.
 
 
 #Actualizaciones Posibles
 
 - Lograr que el script se ejecuta con una temporalidad especifica (cada día o cada cierto tiempo).
-- Implementarlo con mas productos a la vez.
 
 
 #Autor
@@ -50,4 +53,4 @@ Proyecto Realizado como Practica de Automatización para crecer como programador
 
 David Ortega
 
-Versión 1.0.0
+Versión 1.1.0
